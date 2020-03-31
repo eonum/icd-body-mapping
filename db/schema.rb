869159b9811d37b8b4020f0ff83836486df9d0f2
Marius Asadauskas <marius.asadauskas@students.unibe.ch>
2020-03-31 13:02:51 +0200

    added more images to select from

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2020_03_30_123512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ear_elements", force: :cascade do |t|
-    t.string "name"
-    t.string "img"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "icds", force: :cascade do |t|
     t.string "code", null: false
     t.string "version", null: false
