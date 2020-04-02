776eea21ab9e51bc7c24191c3c77de239f069a80
brianmahil <info@jdperformance.ch>
2020-04-02 13:33:21 +0200

    bootstrap

--- app/javascript/components/Topbar.jsx
+++ app/javascript/components/Topbar.jsx
@@ -34,35 +34,52 @@ class Topbar extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
-                <AppBar position="fixed">
-                    <Toolbar variant="dense">
-                        <Grid container spacing={5} alignItems="center" justify="flex-end">
-                            <Grid item>
-                                <input
-                                    type="text"
-                                    className="input"
-                                    placeholder="Search..."
-                                    value={this.state.term}
-                                    onChange={this.getAutoCompleteResults.bind(this)}
-                                />
-                            </Grid>
-                            <Grid item xs />
-                            <Grid item>
-                                <h3>ICD mapping - eonum</h3>
-                            </Grid>
-                            <Grid item>
-                                <Tooltip title="Edit">
-                                    <IconButton color="inherit">
-                                        <EditIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </Grid>
+            <div className="navbar navbar-expand-lg navbar-light bg-primary">
+                <a className="navbar-brand" href="#">ICD Mapping - EONUM</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Ho <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <Grid container spacing={5} alignItems="center" justify="flex-end">
+                        <Grid item>
+                            <input
+                                type="text"
+                                className="input"
+                                placeholder="Search..."
+                                value={this.state.term}
+                                onChange={this.getSearchResults.bind(this)}
+                            />
                         </Grid>
-                    </Toolbar>
-                </AppBar>
-                <Toolbar />
-            </React.Fragment>
+                    </Grid>
+
+                </div>
+            </div>
         )
     }
 }
