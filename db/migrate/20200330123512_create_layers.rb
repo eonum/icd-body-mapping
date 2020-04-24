c2fd6f57a9df6a6c4ac5dca78b426ec8266a5f7d
Marius Asadauskas <marius.asadauskas@students.unibe.ch>
2020-04-24 11:55:01 +0200

    pulled again and added not null to some values

--- db/migrate/20200330123512_create_layers.rb
+++ db/migrate/20200330123512_create_layers.rb
@@ -1,9 +1,9 @@
 class CreateLayers < ActiveRecord::Migration[6.0]
   def change
     create_table :layers do |t|
-      t.string :ebene
-      t.string :name
-      t.string :img
+      t.string :ebene, null: false
+      t.string :name, null: false
+      t.string :img, null: false
 
       t.timestamps
     end
