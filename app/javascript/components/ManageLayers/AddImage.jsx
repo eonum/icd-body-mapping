ef8f01ba6a6005d7556776dd7e322bba969c96e6
saegi95 <aaron.saegesser@gmail.com>
2020-05-25 12:44:11 +0200

    loadicon in add/delete list now always visible and on add of image alphabetical sorting

--- app/javascript/components/ManageLayers/AddImage.jsx
+++ app/javascript/components/ManageLayers/AddImage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import loadingGif from '../../../assets/images/Preloader_2.gif';
 
 class AddImage extends React.Component {
     constructor(props) {
         super(props);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.state = {
+            load: false,
+        };
     }
 
     handleFormSubmit(ebene, name, img) {
         let body = JSON.stringify({layer: {ebene: ebene, name: name, img: img}});
+
+        this.setState({load: true});
         fetch('http://localhost:3000/api/v1/layers', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -15,6 +21,7 @@ class AddImage extends React.Component {
         }).then((layer) => {
             if (layer.ok){
                 this.addNewImage(layer);
+                this.setState({load: false});
             }
         })
     }
@@ -31,34 +38,64 @@ class AddImage extends React.Component {
 
     render() {
         let formFields = {};
+        const loading = this.state.load;
+
+        const loadingImgStyle = {
+            zIndex: 100,
+            position: 'fixed',
+            top: '50%',
+            left: '75%',
+            width: '50px',
+            height: '50px',
+            marginTop: '-25px',
+            marginLeft: '-25px',
+        }
+        const loadingDivStyle = {
+            zIndex: 99,
+            top: '10%',
+            left: '50%',
+            height: '90%',
+            width: '50%',
+            position: 'fixed',
+            backgroundColor: 'rgba(255,255,255,0.7)',
+        }
+        const loadingImg = (
+            <div style={loadingDivStyle}>
+                <img src={loadingGif} style={loadingImgStyle}/>
+            </div>
+        )
+
         return (
-            <form
-                onSubmit={(e) => {
-                    this.handleFormSubmit(formFields.ebene.value, formFields.name.value, formFields.img.value);
-                    e.preventDefault();
-                }}
-                onKeyDown={e => this.handleKeyDown(e)}
-                className="form-group"
-            >
-                <input
-                    ref={input => formFields.ebene = input}
-                    placeholder='Enter the layer E.g. Ohr'
-                    className="form-control"
-                />
-                <input
-                    ref={input => formFields.name = input}
-                    placeholder='Enter the name E.g. Auricula'
-                    className="form-control"
-                />
-                <input
-                    ref={input => formFields.img = input}
-                    placeholder='Enter the image url'
-                    className="form-control"
-                />
-                <button className="btn btn-primary">
-                    Submit
-                </button>
-            </form>
+            <div>
+                {loading ? loadingImg : null}
+                <form
+                    onSubmit={(e) => {
+                        this.handleFormSubmit(formFields.ebene.value, formFields.name.value, formFields.img.value);
+                        e.preventDefault();
+                    }}
+                    onKeyDown={e => this.handleKeyDown(e)}
+                    className="form-group"
+                >
+                    <input
+                        ref={input => formFields.ebene = input}
+                        placeholder='Enter the layer E.g. Ohr'
+                        className="form-control"
+                    />
+                    <input
+                        ref={input => formFields.name = input}
+                        placeholder='Enter the name E.g. Auricula'
+                        className="form-control"
+                    />
+                    <input
+                        ref={input => formFields.img = input}
+                        placeholder='Enter the image url'
+                        className="form-control"
+                    />
+                    <button className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
         );
     }
 }
